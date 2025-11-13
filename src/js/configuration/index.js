cfc97824a58f56f7ea408d3b8981b871b27355bd
MDMCoin <110309662+mdmcoin@users.noreply.github.com>
2025-11-12 21:46:41 -0300

    Update index.js

--- src/js/configuration/index.js
+++ src/js/configuration/index.js
@@ -1,7 +1,5 @@
 import mainnet from './config.mainnet';
 import testnet from './config.testnet';
-import devnet from './config.devnet';
-import stagenet from './config.stagenet';
 
 const configuredNetworks = [mainnet, testnet, stagenet, devnet]
     .filter(item => __NETWORKS__.includes(item.networkId));
